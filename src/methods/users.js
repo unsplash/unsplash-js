f5c856a3dad9ffc21423076141f597bd371e0f18
Sami Jaber <jabersami@gmail.com>
2017-03-31 12:26:21 -0400

    add user stats endpoint & user photo stats param

--- src/methods/users.js
+++ src/methods/users.js
@@ -11,12 +11,19 @@ export default function users(): Object {
       });
     },
 
-    photos: (username: string, page: number = 1, perPage: number = 10, orderBy: string = "latest") => {
+    photos: (
+      username: string,
+      page: number = 1,
+      perPage: number = 10,
+      orderBy: string = "latest",
+      stats: bool = false,
+    ) => {
       const url = `/users/${username}/photos`;
       const query = {
         page,
         per_page: perPage,
-        order_by: orderBy
+        order_by: orderBy,
+        stats
       };
 
       return this.request({
@@ -47,6 +54,20 @@ export default function users(): Object {
         page,
         per_page: perPage,
         order_by: orderBy
+      };
+
+      return this.request({
+        url,
+        method: "GET",
+        query
+      });
+    },
+
+    statistics: (username: string, resolution: string = "days", quantity: number = 30) => {
+      const url = `/users/${username}/statistics`;
+      const query = {
+        resolution,
+        quantity
       };
 
       return this.request({
