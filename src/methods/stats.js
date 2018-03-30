32ce24155a8e60009896abbe34be87adff894aa9
TimmyCarbone <timothy@timmycarbone.com>
2018-03-30 18:23:04 +0200

    Add reporter URL and reportViews method

--- src/methods/stats.js
+++ src/methods/stats.js
@@ -9,6 +9,22 @@ export default function stats(): Object {
         url,
         method: "GET"
       });
+    },
+
+    reportViews: (photoIds) => {
+      const url = "/v";
+
+      const query = {
+        app_id: this._applicationId,
+        photo_id: Array.isArray(photoIds) ? photoIds.join(",") : photoIds
+      };
+
+      return this.request({
+        url,
+        method: "GET",
+        query,
+        reporter: true
+      });
     }
   };
 }
