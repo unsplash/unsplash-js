319fa55c93cad2f369ba4620666c88dc467cd9d7
Luke Chesser <luke.chesser@me.com>
2019-10-25 11:26:34 -0400

    Update examples

--- examples/node/app.js
+++ examples/node/app.js
@@ -2,50 +2,13 @@ import config from 'universal-config';
 import Unsplash, { toJson } from 'unsplash-js';
 
 const unsplash = new Unsplash({
-  accessKey: config.get('ACCESS_KEY'),
-  secret: config.get('SECRET'),
-  callbackUrl: config.get('CALLBACK_URL')
+  accessKey: config.get('ACCESS_KEY')
 });
 
-let authenticationUrl = unsplash.auth.getAuthenticationUrl([
-  "public",
-  "read_user",
-  "write_user",
-  "read_photos",
-  "write_photos",
-  "write_likes",
-  "read_collections",
-  "write_collections"
-]);
-
-console.log(userAuthentication(code));
-currentUser();
 users();
 photos();
 collections();
 stats();
-
-function userAuthentication(code) {
-  return unsplash.auth.userAuthentication(code)
-    .then(toJson)
-    .then(json => json.access_token);
-}
-
-function currentUser() {
-  console.log("\nCurrent User");
-
-  unsplash.currentUser.profile()
-    .then(toJson)
-    .then(json => {
-      console.log('profile', json);
-    });
-
-  unsplash.currentUser.updateProfile({ location: "¯\_(ツ)_/¯" })
-    .then(toJson)
-    .then(json => {
-      console.log('updateProfile', json);
-    });
-}
 
 function users() {
   console.log("\nUsers")
@@ -89,18 +52,6 @@ function photos() {
     .then(json => {
       console.log(json.links.html);
     });
-
-  unsplash.photos.likePhoto("kZ8dyUT0h30")
-    .then(toJson)
-    .then(json => {
-      console.log(json);
-    });
-
-  unsplash.photos.unlikePhoto("kZ8dyUT0h30")
-    .then(toJson)
-    .then(json => {
-      console.log(json);
-    });
 }
 
 function collections() {
@@ -119,36 +70,6 @@ function collections() {
      });
 
    unsplash.collections.getCollectionPhotos(151165)
-     .then(toJson)
-     .then(json => {
-       console.log(json);
-     });
-
-   unsplash.collections.createCollection("Birds", "Wild birds from 'round the world", true)
-     .then(toJson)
-     .then(json => {
-       console.log(json);
-     });
-
-   unsplash.collections.updateCollection(152645, "Wild", "Wild")
-     .then(toJson)
-     .then(json => {
-       console.log(json);
-     });
-
-   unsplash.collections.deleteCollection(152645)
-     .then(toJson)
-     .then(json => {
-       console.log(json);
-     });
-
-   unsplash.collections.addPhotoToCollection(151165, '-yPg8cusGD8')
-     .then(toJson)
-     .then(json => {
-       console.log(json);
-     });
-
-   unsplash.collections.removePhotoFromCollection(151165, '-yPg8cusGD8')
      .then(toJson)
      .then(json => {
        console.log(json);
