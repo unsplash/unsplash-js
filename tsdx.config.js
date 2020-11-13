b7049a098f05463ee6f5f69484d0800259820067
Sami Jaber <jabersami@gmail.com>
2020-11-13 09:18:45 -0500

    add bundle analyzer

---
+++ tsdx.config.js
@@ -0,0 +1,10 @@
+// Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
+const analyzer = require('rollup-plugin-analyzer');
+
+module.exports = {
+  // This function will run for each entry/format/env combination
+  rollup(config, options) {
+    config.plugins.push(analyzer());
+    return config; // always return a config.
+  },
+};
