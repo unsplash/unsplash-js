f1a77241de67d8d412bbcdf43a71e26701600b20
Sami Jaber <jabersami@gmail.com>
2020-11-16 14:59:30 -0500

    extract generateMethod

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,44 @@
 import { flow } from './helpers/fp';
-import { initMakeRequest } from './helpers/request';
+import { initMakeRequest, RequestParams } from './helpers/request';
+import { HandleResponse } from './helpers/response';
 import * as collections from './methods/collections';
 import * as photos from './methods/photos';
 import * as search from './methods/search';
 import * as users from './methods/users';
 
-export const Unsplash = flow(initMakeRequest, makeRequest => ({
-  photos: {
-    get: flow(photos.get.handleRequest, makeRequest(photos.get.handleResponse)),
+export const Unsplash = flow(initMakeRequest, makeRequest => {
+  const generateMethod = <A extends unknown[], B>({
+    handleRequest,
+    handleResponse,
+  }: {
+    handleRequest: (...a: A) => RequestParams;
+    handleResponse: HandleResponse<B>;
+  }) => flow(handleRequest, makeRequest(handleResponse));
 
-    list: flow(
-      photos.list.handleRequest,
-      makeRequest(photos.list.handleResponse),
-    ),
-    getStats: flow(
-      photos.getStats.handleRequest,
-      makeRequest(photos.getStats.handleResponse),
-    ),
-    getRandom: flow(
-      photos.getRandom.handleRequest,
-      makeRequest(photos.getRandom.handleResponse),
-    ),
-    track: flow(
-      photos.track.handleRequest,
-      makeRequest(photos.track.handleResponse),
-    ),
-  },
-  users: {
-    getPhotos: flow(
-      users.getPhotos.handleRequest,
-      makeRequest(users.getPhotos.handleResponse),
-    ),
-    getCollections: flow(
-      users.getCollections.handleRequest,
-      makeRequest(users.getCollections.handleResponse),
-    ),
-    getLikes: flow(
-      users.getLikes.handleRequest,
-      makeRequest(users.getLikes.handleResponse),
-    ),
-    get: flow(users.get.handleRequest, makeRequest(users.get.handleResponse)),
-  },
-  search: {
-    getCollections: flow(
-      search.getCollections.handleRequest,
-      makeRequest(search.getCollections.handleResponse),
-    ),
-    getPhotos: flow(
-      search.getPhotos.handleRequest,
-      makeRequest(search.getPhotos.handleResponse),
-    ),
-    getUsers: flow(
-      search.getUsers.handleRequest,
-      makeRequest(search.getUsers.handleResponse),
-    ),
-  },
-  collections: {
-    getPhotos: flow(
-      collections.getPhotos.handleRequest,
-      makeRequest(collections.getPhotos.handleResponse),
-    ),
-    get: flow(
-      collections.get.handleRequest,
-      makeRequest(collections.get.handleResponse),
-    ),
-    list: flow(
-      collections.list.handleRequest,
-      makeRequest(collections.list.handleResponse),
-    ),
-    getRelated: flow(
-      collections.getRelated.handleRequest,
-      makeRequest(collections.getRelated.handleResponse),
-    ),
-  },
-}));
+  return {
+    photos: {
+      get: generateMethod(photos.get),
+      list: generateMethod(photos.list),
+      getStats: generateMethod(photos.getStats),
+      getRandom: generateMethod(photos.getRandom),
+      track: generateMethod(photos.track),
+    },
+    users: {
+      getPhotos: generateMethod(users.getPhotos),
+      getCollections: generateMethod(users.getCollections),
+      getLikes: generateMethod(users.getLikes),
+      get: generateMethod(users.get),
+    },
+    search: {
+      getCollections: generateMethod(search.getCollections),
+      getPhotos: generateMethod(search.getPhotos),
+      getUsers: generateMethod(search.getUsers),
+    },
+    collections: {
+      getPhotos: generateMethod(collections.getPhotos),
+      get: generateMethod(collections.get),
+      list: generateMethod(collections.list),
+      getRelated: generateMethod(collections.getRelated),
+    },
+  };
+});
