6a79a548fe04f4defcbf0272bbecd8c14993e4be
Sami Jaber <jabersami@gmail.com>
2020-11-16 20:40:04 -0500

    tidy

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,32 @@ import * as photos from './methods/photos';
 import * as search from './methods/search';
 import * as users from './methods/users';
 
-export const createApi = flow(initMakeRequest, makeRequest => {
-  const api = {
-    photos: {
-      get: makeRequest(photos.get),
-      list: makeRequest(photos.list),
-      getStats: makeRequest(photos.getStats),
-      getRandom: makeRequest(photos.getRandom),
-      track: makeRequest(photos.track),
-    },
-    users: {
-      getPhotos: makeRequest(users.getPhotos),
-      getCollections: makeRequest(users.getCollections),
-      getLikes: makeRequest(users.getLikes),
-      get: makeRequest(users.get),
-    },
-    search: {
-      getCollections: makeRequest(search.getCollections),
-      getPhotos: makeRequest(search.getPhotos),
-      getUsers: makeRequest(search.getUsers),
-    },
-    collections: {
-      getPhotos: makeRequest(collections.getPhotos),
-      get: makeRequest(collections.get),
-      list: makeRequest(collections.list),
-      getRelated: makeRequest(collections.getRelated),
-    },
-  };
-
-  return api;
-});
+export const createApi = flow(initMakeRequest, makeRequest => ({
+  photos: {
+    get: makeRequest(photos.get),
+    list: makeRequest(photos.list),
+    getStats: makeRequest(photos.getStats),
+    getRandom: makeRequest(photos.getRandom),
+    track: makeRequest(photos.track),
+  },
+  users: {
+    getPhotos: makeRequest(users.getPhotos),
+    getCollections: makeRequest(users.getCollections),
+    getLikes: makeRequest(users.getLikes),
+    get: makeRequest(users.get),
+  },
+  search: {
+    getCollections: makeRequest(search.getCollections),
+    getPhotos: makeRequest(search.getPhotos),
+    getUsers: makeRequest(search.getUsers),
+  },
+  collections: {
+    getPhotos: makeRequest(collections.getPhotos),
+    get: makeRequest(collections.get),
+    list: makeRequest(collections.list),
+    getRelated: makeRequest(collections.getRelated),
+  },
+}));
 
 export const trackNonHotLinkedPhotoView = ({ appId }: { appId: string }) => ({
   photoId,
