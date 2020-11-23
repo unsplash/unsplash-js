4f1d9deb8421da7acbad863941ce9f77eaea7c30
Sami Jaber <jabersami@gmail.com>
2020-11-23 13:02:10 -0500

    remove react config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    'react-app',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  settings: {
-    react: {
-      version: '999.999.999',
-    },
-  },
+  extends: ['prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   parserOptions: {
     project: ['./tsconfig.json'],
   },
