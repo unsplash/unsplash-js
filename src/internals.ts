e4dd47613c43af62ca5ba03cb4de8bc0181ea8f4
Thomas Lefebvre <thomas@unsplash.com>
2021-06-29 12:14:49 -0400

    Export topic methods

--- src/internals.ts
+++ src/internals.ts
@@ -1,7 +1,8 @@
 import * as collections from './methods/collections';
 import * as photos from './methods/photos';
 import * as search from './methods/search';
+import * as topics from './methods/topics';
 import * as users from './methods/users';
 export { trackNonHotLinkedPhotoView } from './beacon';
 
-export { collections, photos, search, users };
+export { collections, photos, search, users, topics };
