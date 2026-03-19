149367e483abd799a52f1ccaae67ed8939514f5e
Alexandre St-Louis Fortier <alexandre@unsplash.com>
2026-03-19 00:27:28 -0400

    Start from scratch

--- src/internals.ts
+++
@@ -1,8 +0,0 @@
-import * as collections from './methods/collections';
-import * as photos from './methods/photos';
-import * as search from './methods/search';
-import * as topics from './methods/topics';
-import * as users from './methods/users';
-export { trackNonHotLinkedPhotoView } from './beacon';
-
-export { collections, photos, search, users, topics };
